480607a52a8987140673bb519cc8d7e3c2c176c4
mixka <pikopavel@gmail.com>
2025-05-17 18:50:38 +0300

    fix player

--- Source/game/gameCharacter.cpp
+++ Source/game/gameCharacter.cpp
@@ -1,144 +1,102 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "gameCharacter.h"
+#include "gameProjectile.h"
+#include "Animation/AnimInstance.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include "GameFramework/CharacterMovementComponent.h"
+#include "InputActionValue.h"
+#include "Engine/LocalPlayer.h"
 
-gameCharacter::gameCharacter()
+DEFINE_LOG_CATEGORY(LogTemplateCharacter);
+
+//////////////////////////////////////////////////////////////////////////
+// AgameCharacter
+
+AgameCharacter::AgameCharacter()
 {
-    // Настройка коллайдера
-    GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
-    // Создание компонента камеры
-    FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
-    FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
-    FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f));
-    FirstPersonCameraComponent->bUsePawnControlRotation = true;
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
-    // Создание меша персонажа
-    Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
-    Mesh1P->SetOnlyOwnerSee(true);
-    Mesh1P->SetupAttachment(FirstPersonCameraComponent);
-    Mesh1P->bCastDynamicShadow = false;
-    Mesh1P->CastShadow = false;
-    Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
 
-    // Настройка движения
-    if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
-    {
-        MoveComp->MaxWalkSpeed = WalkSpeed;
-        MoveComp->bOrientRotationToMovement = true;
-        MoveComp->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
-    }
-
-    // Инициализация выносливости
-    CurrentStamina = MaxStamina;
-    bIsSprinting = false;
 }
 
-void gameCharacter::BeginPlay()
-{
-    Super::BeginPlay();
+//////////////////////////////////////////////////////////////////////////// Input
 
-    // Добавление маппинга ввода
-    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
-    {
-        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-        {
-            Subsystem->AddMappingContext(DefaultMappingContext, 0);
-        }
-    }
+void AgameCharacter::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
-void gameCharacter::Tick(float DeltaTime)
+void AgameCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-    Super::Tick(DeltaTime);
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
-    // Обновление выносливости
-    if (bIsSprinting)
-    {
-        UpdateStamina(DeltaTime);
-    }
-    else if (CurrentStamina < MaxStamina)
-    {
-        CurrentStamina = FMath::Min(MaxStamina, CurrentStamina + StaminaRecoveryRate * DeltaTime);
-    }
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AgameCharacter::Move);
+
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AgameCharacter::Look);
+	}
+	else
+	{
+		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
 }
 
-void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+
+void AgameCharacter::Move(const FInputActionValue& Value)
 {
-    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
-    {
-        // Прыжок
-        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
 
-        // Движение
-        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
-
-        // Вращение камеры
-        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMyCharacter::Look);
-
-        // Спринт
-        EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Started, this, &AMyCharacter::StartSprint);
-        EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Completed, this, &AMyCharacter::StopSprint);
-    }
+	if (Controller != nullptr)
+	{
+		// add movement 
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
 }
 
-void gameCharacter::Move(const FInputActionValue& Value)
+void AgameCharacter::Look(const FInputActionValue& Value)
 {
-    FVector2D MovementVector = Value.Get<FVector2D>();
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-    if (Controller != nullptr)
-    {
-        AddMovementInput(GetActorForwardVector(), MovementVector.Y);
-        AddMovementInput(GetActorRightVector(), MovementVector.X);
-    }
-}
-
-void gameCharacter::Look(const FInputActionValue& Value)
-{
-    FVector2D LookAxisVector = Value.Get<FVector2D>();
-
-    if (Controller != nullptr)
-    {
-        AddControllerYawInput(LookAxisVector.X);
-        AddControllerPitchInput(LookAxisVector.Y);
-    }
-}
-
-void gameCharacter::StartSprint()
-{
-    if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
-    {
-        const FVector Velocity = MoveComp->Velocity;
-        const float ForwardSpeed = FVector::DotProduct(GetActorForwardVector(), Velocity.GetSafeNormal());
-
-        if (CurrentStamina > 10.0f && ForwardSpeed > 0.1f)
-        {
-            bIsSprinting = true;
-            MoveComp->MaxWalkSpeed = SprintSpeed;
-        }
-    }
-}
-
-void gameCharacter::StopSprint()
-{
-    if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
-    {
-        bIsSprinting = false;
-        MoveComp->MaxWalkSpeed = WalkSpeed;
-    }
-}
-
-void gameCharacter::UpdateStamina(float DeltaTime)
-{
-    CurrentStamina = FMath::Max(0.0f, CurrentStamina - StaminaDrainRate * DeltaTime);
-
-    if (CurrentStamina <= 0.0f)
-    {
-        StopSprint();
-    }
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
